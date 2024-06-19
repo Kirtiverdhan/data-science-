c6905c8a2560532824a1fd58aef9b25cc68b723a
Kirtiverdhan <130557096+Kirtiverdhan@users.noreply.github.com>
2024-06-19 18:31:45 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+np.arange(4)
+
+
+# In[5]:
+
+
+np.zeros(5)
+
+
+# In[20]:
+
+
+np.zeros(7)
+
+
+# In[22]:
+
+
+arr = np.random(8,3)
+
+
+# In[23]:
+
+
+arr
+
+
+# In[24]:
+
+
+np.arange(5,7)
+
+
+# In[26]:
+
+
+dtype()
+
+
+# In[27]:
+
+
+(1+1)*(3+9)
+
+
+# assigen an vallu
+
+# In[28]:
+
+
+vall = 2
+
+
+# In[29]:
+
+
+vall
+
+
+# In[30]:
+
+
+x =2
+y =3
+print(x+y)
+
+
+# In[31]:
+
+
+x = x+x
+
+
+# In[32]:
+
+
+x
+
+
+# In[35]:
+
+
+x = x+5*x
+
+
+# In[36]:
+
+
+x
+
+
+# In[37]:
+
+
+print('this is a string')
+
+
+# In[49]:
+
+
+i = 'iwewuehjnkfk'
+for item in i:
+ print(list(item))
+
+
+# In[51]:
+
+
+num = 18
+name = 'golu'
+print('my name is{}and my age is {}'.format(name,num))
+
+
+# In[52]:
+
+
+2 + 2
+
+
+# In[54]:
+
+
+np.arange(30,20)
+
+
+# In[56]:
+
+
+print('jay siya ram')      
+
+
+# In[ ]:
+
+
+
+
